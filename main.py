55537acb27c6489f3d5ffcdf715f96a513e91656
206 <206@academicos.cesde.edu.co>
2022-02-25 20:46:17 -0500

    NTV001 se crea ejemplo de 3 condiciones

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-#comentario
-print ('que miedo de la guerra')
-
 nivelAgua= int(input('Digita la cantidad de agua de la represa:'))
-print(f'El nivel de agua es: {nivelAgua}')
 
-print ('Oe')
+if (nivelAgua<200):
+    print('No tengo agua')
+elif (nivelAgua>=200 and nivelAgua<450):
+    print('Esta en su punto')
+else: 
+    print('Nos vamos a morir')    
+
